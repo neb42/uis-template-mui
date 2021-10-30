1ea48337fca29874cf8711f9d705051efaa78ff8
Ben McAlindin <bmcalindin@gmail.com>
2021-10-30 15:15:55 +0100

    Import roboto font

--- App.tsx
+++ App.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
 
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
+
 const App = ({ children }: any) => children;
 
 export default App;
